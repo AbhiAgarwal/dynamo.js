06224be158a18e6894e19ef04114c67bd6cf8f40
Abhi Agarwal <me@abhiagarwal.com>
2014-04-28 13:14:37 -0400

    Improving code

---
+++ examples/helloworld.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var dynamo = require('../lib/core');
+
+// Initialization
+dynamo.initialize();
+
+// Router
+dynamo.route('/', function(req, res){
+    res.send('hello world');
+});
+
+// Run
+dynamo.run();
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 'use strict';
 
 var dynamo = require('./lib/core');
-
-// Initialization
-dynamo.initialize();
-
-// Router
-dynamo.route('/', function(req, res){
-    res.send('hello world');
-});
-
-// Run
-dynamo.run();
