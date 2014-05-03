7bb254ec9872f72e164011bb06e9c422af6e4f49
Abhi Agarwal <me@abhiagarwal.com>
2014-05-03 01:19:59 -0400

    Adding Form with csrf example

---
+++ examples/form.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var dynamo = require('../lib/core');
+
+// Initialization
+dynamo.initialize(__dirname);
+
+// Router
+// This particular dust file includes a '_csrf' field within it.
+// You can enable/disable in the development.json file & read upon it.
+dynamo.route('/', function(req, res) {
+    res.render('form-example');
+});
+
+// Post Form
+// Gets the values of the inputs from the particular files.
+dynamo.app.post('/submitForm', function(request, response, next){
+    console.log(request.body.user.name);
+    console.log(request.body.user.email);
+    next();
+});
+
+// Run
+dynamo.run();
